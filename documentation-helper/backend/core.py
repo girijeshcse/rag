a86bb75003a9e3f6608797d0f11270a451593a29
Girijesh Prasad <girijeshcse@gmail.com>
2023-08-14 18:00:27 +0530

    Add files via upload

---
+++ documentation-helper/backend/core.py
@@ -0,0 +1,44 @@
+import os
+from dotenv import load_dotenv
+from typing import Any, Dict, List
+
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import Pinecone
+from langchain.chains import ConversationalRetrievalChain
+import pinecone
+
+
+load_dotenv()
+
+INDEX_NAME = "langchain-doc-index"
+
+pinecone.init(
+    api_key=os.environ["PINECONE_API_KEY"],
+    environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
+)
+
+
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
+    embeddings = OpenAIEmbeddings()
+    docsearch = Pinecone.from_existing_index(
+        index_name=INDEX_NAME, embedding=embeddings
+    )
+    chat = ChatOpenAI(verbose=True, temperature=0)
+    # qa = RetrievalQA.from_chain_type(
+    #     llm=chat,
+    #     chain_type="stuff",
+    #     retriever=docsearch.as_retriever(),
+    #     return_source_documents=True,
+    # )
+
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
+        )
+    return qa({"question": query, "chat_history": chat_history})
+
+
+if __name__ == "__main__":
+    print(run_llm(query="What is LangChain?"))
